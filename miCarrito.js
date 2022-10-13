7ca5fcc5600d52623fe03fbbf5d0967962c125a5
wilmar <wilmarandresrojasvillafane@gmail.com>
2022-10-12 23:06:34 -0500

    Eliminar Product Carrito

--- miCarrito.js
+++ miCarrito.js
@@ -1,27 +1,37 @@
-var total = 0
 
 document.addEventListener("DOMContentLoaded", () => {
     
+    imprimirCarrito();
+    
+})
+
+function imprimirCarrito(){
+
+    let total = 0;
+
     let localCarrito = localStorage.getItem("carrito");
 
     let datosCarrito = JSON.parse(localCarrito);
 
-    datosCarrito.forEach(producto => {
+    document.getElementById("listar").innerHTML = " ";
+
+    datosCarrito.forEach(producto  => {
      let row = `
             <tr>
                 <td>
-                    <h6 class="title-elipsis" style="padding-top: 30px;">${producto.id}</h6>
+                    <h6 class="title-elipsis" style="padding-top: 40px;">${producto.id}</h6>
                 </td>
                 <td>                    
                     <img src="${producto.imagen}" class="img-fluid rounded-start" alt="Image Product" style="padding: 5px; width: 120px;" >                    
                 </td>
                 <td>
-                    <h6 class="title-elipsis" style="padding-top: 30px;">${producto.titulo}</h6>
+                    <h6 class="title-elipsis" style="padding-top: 40px;">${producto.titulo}</h6>
                 </td> 
                 <td>
-                    <h5 class="text-success" style="padding-top: 30px;">$ ${(producto.precio).toLocaleString('en-US')}</h5>                
+                    <h5 class="text-success" style="padding-top: 40px;">$ ${(producto.precio).toLocaleString('en-US')}</h5>                
                 </td> 
-                <td>
+                <td style="padding-top: 40px;">
+                     <button type="button" class="btn btn-danger" onclick=eliminar('${producto.id}')>Eliminar</button>
                 </td>
             </tr>`   
             
@@ -32,6 +42,28 @@ document.addEventListener("DOMContentLoaded", () => {
        document.getElementById("listar").innerHTML += row;
     });
 
-    
-})
+}
+
+function eliminar(id){
+   
+    let localCarrito = localStorage.getItem("carrito");
+
+    let datosCarrito = JSON.parse(localCarrito);
+
+    console.log(datosCarrito)
+
+    for(let i = 0; i < datosCarrito.length; i++){
+        
+        if(datosCarrito[i].id == id){
+           datosCarrito.splice(i, 1);
+           
+        }
+    }
+
+    localStorage.setItem("carrito", JSON.stringify(datosCarrito));
+
+    imprimirCarrito();
+
+}
+
 
